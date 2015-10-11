d01a94db1f8f5749b6408d6837b0a4cafdd2ed1d
nigel <nmillward@tripadvisor.com>
2015-10-11 21:24:08 +0100

    more code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function()
 {
+	$('.buttonClass').hide();
+
 	// Questions
-	var question = ['England', 
+	var qArray = ['England', 
 	'Italy', 
 	'America', 
 	'Australia', 
 	'Japan', 
 	'India' ];
 
-	var answers = [
+	var aArray = [
 		['Manchester', 'London', 'Oxford', 'Birmingham', 'Leeds'],
 		['Milan', 'Naples', 'Rome', 'Turin', 'Cagliari'],
 		['Boston', 'Lincoln','Olympia','Washington DC', 'Austin'],
@@ -17,21 +19,62 @@ $(document).ready(function()
 		['Mumbai','Hyderabad', 'Delhi','Kolkata', 'Bangalore']
 	]
 
-	var index = 0;
+	// Index 
+	var index = -1
+	function addIndex(index){
+		return index += +1;
+	}
+
+	function quizAnswers(index)
+	{
+		for(i = addIndex(index); i < 6; i++) 
+		{
+   			$('.Answers').append('<input type="radio" name="radio_name" />' + aArray[index][i] + ' <br>');
+		}
+
+	}
+
+	function checkAnswers()
+	{
+
+	}
+
+	var questionNum = -1;
+	var numCorrect = 0;
 
 	// Start the quiz
 	$('.Start').on('click', function()
 	{
-		var html = '<p>' + 'What is the capital city of' + question[index] + '?</p>';
-		for(i = 0; i < answers[index].length; i++)
+		// remove congratulations text if quiz reran
+		$('.Questions p').hide();
+
+
+		var question = 'What is the capital city of ' + qArray[addIndex(index)] + '?'
+		$('.Questions').append('<p>' + question + '</p>');
+		quizAnswers(index);
+		$('.Start').hide();
+		$('.buttonClass').show();
+		index++;
+	});
+
+	// Click on the next button
+	$('.buttonClass').on('click',function()
+	{
+		$('.Questions p').hide();
+		$('.Answers p').hide();
+
+		if(index < (qArray.length -1))
 		{
-			html += '<label><input type="radio" name="ans" value="' 
-            + answers[index][i] + '"/ > ' + answers[index][i] + ' </label>';
+			var question = 'What is the capital city of ' + qArray[addIndex(index)] + '?'
+			$('.Questions').append('<p>' + question + '</p>');
+			quizAnswers(index);
+			index++;
+		} else
+		{
+			$('.buttonClass').hide();
+			$('.Start').show();
+			$('.Questions').append('<p> Congratulations! You have completed the quiz </p>');
+			index = -1;
 		}
-
-		html += '</div > ';
-    	document.getElementsByClassName('.Questions').innerHTML = html;
-
-		index++
 	});
 });
