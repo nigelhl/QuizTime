3ddd9dc0e962252e3b3ed6d663d26e04c4bf9fcc
nigel <nmillward@tripadvisor.com>
2015-10-11 12:12:45 +0100

    more code...

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
+	// Questions
+	var question = ['England', 
+	'Italy', 
+	'America', 
+	'Australia', 
+	'Japan', 
+	'India' ];
 
-var questions = butt
+	var answers = [
+		['Manchester', 'London', 'Oxford', 'Birmingham', 'Leeds'],
+		['Milan', 'Naples', 'Rome', 'Turin', 'Cagliari'],
+		['Boston', 'Lincoln','Olympia','Washington DC', 'Austin'],
+		['Adelaide', 'Melbourne', 'Brisbane', 'Sydney', 'Perth'],
+		['Yokohama','Kyoto','Hiroshima', 'Fukuoka', 'Tokyo'],
+		['Mumbai','Hyderabad', 'Delhi','Kolkata', 'Bangalore']
+	]
 
+	var index = 0;
 
-
-// Start the quick
+	// Start the quiz
 	$('.Start').on('click', function()
 	{
+		var html = '<p>' + 'What is the capital city of' + question[index] + '?</p>';
+		for(i = 0; i < answers[index].length; i++)
+		{
+			html += '<label><input type="radio" name="ans" value="' 
+            + answers[index][i] + '"/ > ' + answers[index][i] + ' </label>';
+		}
 
-	}
+		html += '</div > ';
+    	document.getElementsByClassName('.Questions').innerHTML = html;
 
-
-
-)};
+		index++
+	});
+});
